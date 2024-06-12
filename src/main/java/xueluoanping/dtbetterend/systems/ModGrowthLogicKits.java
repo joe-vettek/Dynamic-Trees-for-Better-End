35aa1ca4ed0824cc91ae6d6bdd34f968fb0172db
joe-vettek <my1233215@126.com>
2024-06-12 17:31:26 +0800

    update

--- src/main/java/xueluoanping/dtbetterend/systems/ModGrowthLogicKits.java
+++ src/main/java/xueluoanping/dtbetterend/systems/ModGrowthLogicKits.java
@@ -8,13 +8,17 @@ import xueluoanping.dtbetterend.systems.growthlogic.*;
 
 
 public class ModGrowthLogicKits {
-    public static final GrowthLogicKit HEMLOCK = new RedwoodLogic(new ResourceLocation(DTBetterEnd.MOD_ID, "hemlock"));
     public static final GrowthLogicKit WISTERIA = new TenaneaTreeLogic(new ResourceLocation(DTBetterEnd.MOD_ID, "tenanea"));
-    public static final GrowthLogicKit LUCERNIA_TREE_LOGIC = new LucerniaTreeLogic(DTBetterEnd.rl("lucernia"));
+    public static final GrowthLogicKit LUCERNIA = new LucerniaTreeLogic(DTBetterEnd.rl("lucernia"));
+    public static final GrowthLogicKit DRAGON_TREE = new DragonTreeLogic(DTBetterEnd.rl("dragon_tree"));
+    public static final GrowthLogicKit LACUGROVE = new LacugroveTreeLogic(DTBetterEnd.rl("lacugrove"));
+    public static final GrowthLogicKit PYTHADENDRON = new PythadendronTreeLogic(DTBetterEnd.rl("pythadendron"));
 
     public static void register(final Registry<GrowthLogicKit> registry) {
-        registry.registerAll(HEMLOCK);
         registry.register(WISTERIA);
-        registry.register(LUCERNIA_TREE_LOGIC);
+        registry.register(LUCERNIA);
+        registry.register(DRAGON_TREE);
+        registry.register(LACUGROVE);
+        registry.register(PYTHADENDRON);
     }
 }
